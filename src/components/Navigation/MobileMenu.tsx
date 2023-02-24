a94c89c0c8b7f5fff1599604eed433057928b430
yousaf wazir <umbrellaman612@gmail.com>
2023-02-24 09:56:51 +0000

    added skeleton mobile nav

---
+++ src/components/Navigation/MobileMenu.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
+export default function MobileMenu() {
+  return (
+    <button className="w-auto h-auto">
+      <AiOutlineMenu className="text-5xl"/>
+    </button>
+  );
+}
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+
 export default function Navigation() {
   return (
     <>
-      <nav>Nav</nav>
+      <nav className="h-24 border flex items-center justify-between px-5">
+        {/* Logo */}
+        <Link className="relative w-16 h-10" href="/">
+          <Image src="/images/logo.svg" alt="Logo" fill />
+        </Link>
+
+        <MobileMenu />
+      </nav>
     </>
   );
 }

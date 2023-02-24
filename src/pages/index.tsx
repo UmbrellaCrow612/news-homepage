0bb8dab2f59839d762bef77983c05a65ab46ef2a
yousaf wazir <umbrellaman612@gmail.com>
2023-02-24 12:14:40 +0000

    added the mobile view for hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -9,7 +12,66 @@ export default function Page() {
         {/* Future add og tags and other seo stuff */}
       </Head>
 
-      <div id="#main-content">Homepage</div>
+      <div
+        id="main-content"
+        className="grid grid-cols-1 grid-rows-2 h-auto border px-8"
+      >
+        {/* Left  */}
+        <div className="flex flex-col items-start gap-5">
+          {/* Image wrapper */}
+          <div className="w-full h-[20rem] relative">
+            <Image
+              fill
+              src="/images/image-web-3-desktop.jpg"
+              alt="Image"
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+
+          <h1 className="font-bold text-6xl">The Bright Future of Web 3.0?</h1>
+          <p className="text-gray-500">
+            We dive into the next evolution of the web that claims to put the
+            power of the platform back into the hands of the people. But is it
+            really fulfilling its promise
+          </p>
+          <Link className="btn btn-primary" href="/">
+            Read More
+          </Link>
+        </div>
+        {/* Right  */}
+        <div className="flex items-center justify-center py-20">
+          <div className="bg-black h-full w-full p-5 text-white flex flex-col gap-4">
+            <p className="font-bold text-yellow-500 text-4xl">NEW</p>
+            <div className="flex flex-col gap-5 divide-y divide-slate-500">
+              {/* Block */}
+              <div className="p-3 flex flex-col gap-3">
+                <h2 className="text-white font-bold text-2xl leading-tight tracking-wide">
+                  Hydrogen VS Electric Cars
+                </h2>
+                <p className="text-gray-400 text-lg font-light">
+                  Will Hydrogen-Fueled cars ever catch up to EV&apos;s?
+                </p>
+              </div>
+              <div className="p-3 flex flex-col gap-3">
+                <h2 className="text-white font-bold text-2xl leading-tight tracking-wide">
+                  The downsides of AI Artistry
+                </h2>
+                <p className="text-gray-400 text-lg font-light">
+                  What are the possible adverse effects of on-demand AI image generation?
+                </p>
+              </div>
+              <div className="p-3 flex flex-col gap-3">
+                <h2 className="text-white font-bold text-2xl leading-tight tracking-wide">
+                  Is VC funding drying up?
+                </h2>
+                <p className="text-gray-400 text-lg font-light">
+                  Private funding by VC firms is down 50% YOY. We take a look at what that means.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

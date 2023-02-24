273fc02e6592c6c083c50a8bfb4ad201181a527d
yousaf wazir <umbrellaman612@gmail.com>
2023-02-24 09:36:09 +0000

    added font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  preload: true,
+});
+
 export default function Page() {
-  return <>Homepage</>
+  return <div className={`${inter.className}`}>Homepage</div>;
 }

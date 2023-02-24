fbb49a82bfee1bc771dbf9d89edde544e63677d3
yousaf wazir <umbrellaman612@gmail.com>
2023-02-24 12:25:10 +0000

    added desktop hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,10 @@ export default function Page() {
 
       <div
         id="main-content"
-        className="grid grid-cols-1 grid-rows-2 h-auto border px-8"
+        className="grid gap-6 grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-3 h-auto px-8 lg:px-6"
       >
         {/* Left  */}
-        <div className="flex flex-col items-start gap-5">
+        <div className="flex flex-col items-start gap-5 col-span-2">
           {/* Image wrapper */}
           <div className="w-full h-[20rem] relative">
             <Image
@@ -28,18 +28,24 @@ export default function Page() {
             />
           </div>
 
-          <h1 className="font-bold text-6xl">The Bright Future of Web 3.0?</h1>
-          <p className="text-gray-500">
-            We dive into the next evolution of the web that claims to put the
-            power of the platform back into the hands of the people. But is it
-            really fulfilling its promise
-          </p>
-          <Link className="btn btn-primary" href="/">
-            Read More
-          </Link>
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
+            <h1 className="font-bold text-6xl">
+              The Bright Future of Web 3.0?
+            </h1>
+            <div className="px-4 py-3 flex flex-col justify-between items-start">
+              <p className="text-gray-500">
+                We dive into the next evolution of the web that claims to put
+                the power of the platform back into the hands of the people. But
+                is it really fulfilling its promise
+              </p>
+              <Link className="btn btn-primary" href="/">
+                Read More
+              </Link>
+            </div>
+          </div>
         </div>
         {/* Right  */}
-        <div className="flex items-center justify-center py-20">
+        <div className="flex items-center justify-center py-20 lg:py-0">
           <div className="bg-black h-full w-full p-5 text-white flex flex-col gap-4">
             <p className="font-bold text-yellow-500 text-4xl">NEW</p>
             <div className="flex flex-col gap-5 divide-y divide-slate-500">
@@ -57,7 +63,8 @@ export default function Page() {
                   The downsides of AI Artistry
                 </h2>
                 <p className="text-gray-400 text-lg font-light">
-                  What are the possible adverse effects of on-demand AI image generation?
+                  What are the possible adverse effects of on-demand AI image
+                  generation?
                 </p>
               </div>
               <div className="p-3 flex flex-col gap-3">
@@ -65,7 +72,8 @@ export default function Page() {
                   Is VC funding drying up?
                 </h2>
                 <p className="text-gray-400 text-lg font-light">
-                  Private funding by VC firms is down 50% YOY. We take a look at what that means.
+                  Private funding by VC firms is down 50% YOY. We take a look at
+                  what that means.
                 </p>
               </div>
             </div>

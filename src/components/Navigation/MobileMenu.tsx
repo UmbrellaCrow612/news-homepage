499fae3e180466cfff5df45865cfb8c1e47a962f
yousaf wazir <umbrellaman612@gmail.com>
2023-02-24 11:19:34 +0000

    menu dose not show on bigger screens

--- src/components/Navigation/MobileMenu.tsx
+++ src/components/Navigation/MobileMenu.tsx
@@ -2,7 +2,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 export default function MobileMenu() {
   return (
-    <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost">
+    <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost lg:hidden">
       <AiOutlineMenu className="text-3xl"/>
     </label>
   );
